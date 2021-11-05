a1327a2deda21d2377dd98b236393238a12a25cd
anshchawla521 <anshchawla521@gmail.com>
2021-11-06 00:06:52 +0530

    organisied a little , added readme

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 from typing import Text
 import telebot
 import time
-import credentials
+import credentials 
+
 
 bot_token=credentials.bot_token
-
 bot = telebot.TeleBot(token = bot_token)
 
 def findat(msg):
-    # from a list of texts, it finds the one with the '@' sign
-    for i in msg:
-        if '@' in i:
-            return i
+        # from a list of texts, it finds the one with the '@' sign
+        for i in msg:
+            if '@' in i:
+                return i
 
-@bot.message_handler(commands=['Greet'])
-def send_welcome(message):
-    bot.reply_to(message,'welcome')
+def get_message():
+    @bot.message_handler(commands=['Greet'])
+    def send_welcome(message):
+        bot.reply_to(message,'welcome')
 
 
-@bot.message_handler(commands=['Hello'])
-def hello(message):
-    bot.send_message(message.chat.id,'Hey,How are You??')
+    @bot.message_handler(commands=['Hello'])
+    def hello(message):
+        bot.send_message(message.chat.id,'Hey,How are You??')
 
-@bot.message_handler(commands=['PEC'])
-def celeb(message):
-    bot.reply_to(message,'Celebrating 100 years')
+    @bot.message_handler(commands=['PEC'])
+    def celeb(message):
+        bot.reply_to(message,'Celebrating 100 years')
 
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
-def at_answer(message):
-    texts=message.text.split()
-    at_text=findat(texts)
+    @bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+    def at_answer(message):
+        texts=message.text.split()
+        at_text=findat(texts)
 
-    if at_text == '@': # in case it's just the '@', skip
-        pass
-    else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
-        bot.reply_to(message, insta_link)
+        if at_text == '@': # in case it's just the '@', skip
+            pass
+        else:
+            insta_link = "https://instagram.com/{}".format(at_text[1:])
+            bot.reply_to(message, insta_link)
 
-bot.polling()
-    
+    bot.polling()
+        
 
+if __name__ == '__main__':
+    get_message() # implement a loop so that this bot checks for messages every 10 seconds or maybe multi threading
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Led matrix using RPi ,ws2811 and telegram bot api
+
+> A file by the name credentails.py must be placed inside the root directory of project. The contents of this file should be `bot_token = 'Your bot token'`
