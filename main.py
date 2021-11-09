08d460bd3db9a5dabf514a4ed61d4d324b84e9e4
shubham <singla16shubham@gmail.com>
2021-11-09 12:59:24 +0530

    organised and improved interface of telegram bot

--- main.py
+++ main.py
@@ -3,43 +3,73 @@ import telebot
 import time
 import credentials 
 
+h=0
+messages_list=[]
+s=set()
+quotes = [
+    '1. Celebarting 100th aniversary of PEC',
+    '2. Robotics society',
+    '3. Welcome President of INDIA',
+    '4. Before software can be reusable it first has to be usable. – Ralph Johnson',
+    '5. Optimism is an occupational hazard of programming: feedback is the treatment. - Kent Beck'
+]
+# loop through the quotes
+
 
 bot_token=credentials.bot_token
 bot = telebot.TeleBot(token = bot_token)
 
-def findat(msg):
-        # from a list of texts, it finds the one with the '@' sign
-        for i in msg:
-            if '@' in i:
-                return i
+def check_len():
+    '''this function is checking the length of the message list if exceedes 1000 then delete 500 elements '''
+    if(len(messages_list)>1000):# to be verified by ans we are deleting element once read by LED
+        #have to be deleted in the set also
+        
+        while len(messages_list)>500:
+           a= messages_list.pop(0)
+           s.remove(a[1])
+           
+
+    pass
 
 def get_message():
-    @bot.message_handler(commands=['Greet'])
-    def send_welcome(message):
-        bot.reply_to(message,'welcome')
+  
+   # @bot.message_handler(commands=['Quote'])
+    
 
-
-    @bot.message_handler(commands=['Hello'])
-    def hello(message):
-        bot.send_message(message.chat.id,'Hey,How are You??')
-
-    @bot.message_handler(commands=['PEC'])
-    def celeb(message):
-        bot.reply_to(message,'Celebrating 100 years')
-
-    @bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
-    def at_answer(message):
-        texts=message.text.split()
-        at_text=findat(texts)
-
-        if at_text == '@': # in case it's just the '@', skip
-            pass
+    @bot.message_handler(commands=['Start','start'])
+    def star_bot(message):
+        for quote in quotes:
+            bot.send_message(message.chat.id,quote)
+                 
+        bot.send_message(message.chat.id,"If you want to print 1st message then send /1 and same for other options.")
+   
+    
+    
+    
+    @bot.message_handler(commands=['1','2','3'])
+    def send_1(message):
+        h=message.json['text'][1:]
+        
+        if(message.chat.id not in s):
+            s.add(message.chat.id)
+            
+            bot.reply_to(message,'Your Request is accepted')
+            a=[int(h),message.chat.id]
+            messages_list.append(a)
         else:
-            insta_link = "https://instagram.com/{}".format(at_text[1:])
-            bot.reply_to(message, insta_link)
+            bot.reply_to(message,'Your request is alredy in queue')
+        check_len()
+        print(messages_list)
+    
+    
+    
+  
 
+    
+   
     bot.polling()
         
 
 if __name__ == '__main__':
     get_message() # implement a loop so that this bot checks for messages every 10 seconds or maybe multi threading
+    check_len
